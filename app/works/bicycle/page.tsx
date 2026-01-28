44f061bfa1c21e2ddd8d9b4c8324df70ac60c565
TakumiU <takumiu@TakumiUnoMacBook-Air.local>
2026-01-28 16:36:53 +0900

    asd

--- app/works/bicycle/page.tsx
+++ app/works/bicycle/page.tsx
@@ -10,7 +10,7 @@ export default function WorkDetailPage({
     <section className="mx-auto max-w-3xl space-y-6">
         <div className="relative h-64 w-full sm:h-80">
             <Image
-    src="/works/bicycle.png"
+    src="/works/bicycle.jpeg"
     alt="Bicycle"
     fill
     priority
@@ -21,6 +21,8 @@ export default function WorkDetailPage({
       <h1 className="text-3xl font-bold">{params.slug}</h1>
       <p className="text-gray-700">
         大学２年の夏に長野から京都までの約310kmを中山道を通って自転車で移動。
+      </p>
+      <p className="text-gray-700">
         低コストかつスムーズに移動できるように、事前に1日の行動計画を立てて実行した。予想外のトラブルが生じたとしても計画のおかげで迅速に対処することができ、計画の重要性を感じた。
       </p>
     </section>
