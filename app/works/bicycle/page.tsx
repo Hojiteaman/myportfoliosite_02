b4172b2958c9ae453d62bb0ee54514675c508ffe
TakumiU <takumiu@TakumiUnoMacBook-Air.local>
2026-02-02 05:30:48 +0900

    fin

--- app/works/bicycle/page.tsx
+++ app/works/bicycle/page.tsx
@@ -30,10 +30,10 @@ export default function WorkDetailPage({
                     </div>
                     <p className="text-center text-gray-400">京都までの旅路</p>
       <p className="text-gray-700">
-        大学２年の夏に長野から京都までの300km以上の道のりを中山道を通って自転車で移動。<br/>夏真っ只中のお盆の時期でありましたが、5日間は天候に恵まれました。
+        　大学２年の夏に長野から京都までの300km以上の道のりを中山道を通って自転車で移動。夏真っ只中のお盆の時期でありましたが、5日間は天候に恵まれました。
       </p>
       <p className="text-gray-700">
-        低コストかつスムーズに移動できるように、事前に1日の行動計画を立てて実行に移しました。<br />予想外のトラブルが生じたとしても計画のおかげで迅速に対処することができ、計画の重要性を感じた旅となりました。
+        　低コストかつスムーズに移動できるように、事前に1日の行動計画を立てて実行に移しました。予想外のトラブルが生じたとしても計画のおかげで迅速に対処することができ、計画の重要性を感じた旅となりました。
       </p>
 
       <div className="relative h-64 w-full sm:h-80">
@@ -46,7 +46,7 @@ export default function WorkDetailPage({
                             />
                     </div>
                     <p className="text-center text-gray-400">野宿の様子</p>
-      <p className="text-gray-700">道中は公園にテントを立てて、野宿しました。近くに入浴施設やお店があるような場所を事前に選んだため、快適に過ごすことができました。<br/>一日大体70kmは走って体がクタクタだったので、寝床を用意したらすぐに寝れました。</p>
+      <p className="text-gray-700">　道中は公園にテントを立てて、野宿しました。近くに入浴施設やお店があるような場所を事前に選んだため、快適に過ごすことができました。<br/>一日大体70kmは走って体がクタクタだったので、寝床を用意したらすぐに寝れました。</p>
       <div className="relative h-64 w-full sm:h-80">
             <Image
                             src="/works/view.jpeg"
@@ -57,7 +57,7 @@ export default function WorkDetailPage({
                             />
                     </div>
                     <p className="text-center text-gray-400">早朝の寝覚めの床</p>
-      <p className="text-gray-700">木曽路の景勝地の一つ「寝覚めの床」の早朝の様子。<br />熱中症対策として早朝から移動していましたが、そのおかげで美しい景色を独り占めできました。</p>
+      <p className="text-gray-700">　木曽路の景勝地の一つ「寝覚めの床」の早朝の様子。<br />熱中症対策として早朝から移動していましたが、そのおかげで美しい景色を独り占めできました。</p>
     </section>
   )
 }
--- app/works/pbl/page.tsx
+++ app/works/pbl/page.tsx
@@ -21,7 +21,7 @@ export default function WorkDetailPage({
       <h1 className="text-3xl text-gray-700 font-bold">PBL</h1>
       <p className="font-semibold text-gray-900">PBLの内容</p>
       <p className="text-gray-700">
-　フランス人留学生とグループを組み、特定のテーマに沿ったVRプロジェクトを製作し、発表するイベント。<br />私を含む日本人2人、フランス人2人でチームを組み、学部3年の5月から8月まで取り組みました。<br />
+　フランス人留学生とグループを組み、特定のテーマに沿ったVRプロジェクトを製作し、発表するイベント。私を含む日本人2人、フランス人2人でチームを組み、学部3年の5月から8月まで取り組みました。<br />
 </p>
       <p className="font-semibold text-gray-900">取り組んだ内容</p>
       <p className="text-gray-700">
