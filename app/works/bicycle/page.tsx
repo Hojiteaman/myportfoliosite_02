34165b26483e2b30e1924d003755dc28ef43ae02
TakumiU <takumiu@TakumiUnoMacBook-Air.local>
2026-02-02 02:52:14 +0900

    jo

--- app/works/bicycle/page.tsx
+++ app/works/bicycle/page.tsx
@@ -19,8 +19,18 @@ export default function WorkDetailPage({
         </div>
         
       <h1 className="text-3xl font-bold">{params.slug}</h1>
+      <div className="relative h-64 w-full sm:h-80">
+            <Image
+                            src="/works/bicycleroad.jpeg"
+                            alt="image bicycle road"
+                            fill
+                            priority
+                            className="object-cover"
+                            />
+                    </div>
+                    <p className="text-center text-gray-700">京都までの旅路</p>
       <p className="text-gray-700">
-        大学２年の夏に長野から京都までの約310kmを中山道を通って自転車で移動。
+        大学２年の夏に長野から京都までの300km以上の道のりを中山道を通って自転車で移動。<br/>夏真っ只中のお盆の時期でありましたが、5日間は天候に恵まれました。
       </p>
       <p className="text-gray-700">
         低コストかつスムーズに移動できるように、事前に1日の行動計画を立てて実行に移しました。<br />予想外のトラブルが生じたとしても計画のおかげで迅速に対処することができ、計画の重要性を感じた旅となりました。
@@ -36,7 +46,7 @@ export default function WorkDetailPage({
                             />
                     </div>
                     <p className="text-center text-gray-700">野宿の様子</p>
-      <p className="text-gray-700">道中は公園にテントを立てて、野宿しました。近くに入浴施設やお店があるような場所を事前に選んだため、快適に過ごすことができました。</p>
+      <p className="text-gray-700">道中は公園にテントを立てて、野宿しました。近くに入浴施設やお店があるような場所を事前に選んだため、快適に過ごすことができました。<br/>一日大体70kmは走って体がクタクタだったので、寝床を用意したらすぐに寝れました。</p>
       <div className="relative h-64 w-full sm:h-80">
             <Image
                             src="/works/view.jpeg"
@@ -47,7 +57,7 @@ export default function WorkDetailPage({
                             />
                     </div>
                     <p className="text-center text-gray-700">早朝の寝覚めの床</p>
-      <p className="text-gray-700">木曽路の景勝地の一つ「寝覚めの床」の早朝の様子。<br />熱中症対策で早朝から移動していましたが、そのおかげで美しい景色を独り占めできました。</p>
+      <p className="text-gray-700">木曽路の景勝地の一つ「寝覚めの床」の早朝の様子。<br />熱中症対策として早朝から移動していましたが、そのおかげで美しい景色を独り占めできました。</p>
     </section>
   )
 }
--- app/works/france/page.tsx
+++ app/works/france/page.tsx
@@ -25,6 +25,18 @@ export default function WorkDetailPage({
         残りの1週間はレンヌとパリを観光し、宿の手配や交通手段の確保、観光地チケットの購入などもメンバーで役割分担して行いました。
         私はパリの宿泊先探しを担当し、治安が良く学生でも利用しやすい価格帯の宿を探しました。<br />また、荷物預かりが無料と記載されているにもかかわらず現地で有料請求されるケースがあることを想定し、事前にメールでオプションの確認を実施しました。実際に同様の事例に遭遇しましたが、事前のメールが証拠となり問題なく対応することができました。
       </p>
+      <div></div>
+      <div className="relative h-64 w-full sm:h-80">
+      <Image
+                      src="/works/homestay.png"
+                      alt="image homestay"
+                      fill
+                      priority
+                      className="object-cover"
+                      />
+              </div>
+              <p className="text-center text-gray-700">ホームファミリーとの写真</p>
+              <p className="text-gray-700">留学中に誕生日を迎えた私に、ホームファミリーがサプライズでケーキを用意してくれました。<br />あまりの驚きに、泣きそうになっています。</p>
     </section>
   )
 }
